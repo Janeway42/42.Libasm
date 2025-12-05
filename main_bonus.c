71455b26820c1058b87f4c41be1f3020f2079d24
Janeway <corina.a.popa@gmail.com>
2025-12-05 17:44:43 +0100

    work in progress

--- main_bonus.c
+++ main_bonus.c
@@ -4,13 +4,18 @@
 #include <errno.h>
 
 //declare assembly functions and errno
-extern size_t ft_strlen(const char *s);
-extern int ft_strcmp(const char *s1, const char *s2);
-extern char *ft_strcpy(char *dest, const char *src);
-extern ssize_t ft_write(int fd, const void *buf, size_t count);
-extern ssize_t ft_read(int fd, void *buf, size_t count);
-extern char * ft_strdup(const char *str1);
+extern int ft_atoi_base(char *str, char *base)
+extern void ft_list_push_front(t_list **begin_list, void *data);
+extern void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *));
+extern int ft_list_size(t_list *begin_list);
+extern void ft_list_sort(t_list **begin_list, int (*cmp)());
 extern int errno;
+
+typedef struct  s_list
+{
+    void *data;
+    struct s_list *next;
+}               t_list;
 
 void *free_fct(void *data)
 {
